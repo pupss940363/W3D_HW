f89a77deb56b35cd2a0c60dcf213db05eb89e0dd
pupss9403630 <32891820+pupss940363@users.noreply.github.com>
2022-12-25 16:35:31 +0800

    Create OrbitControls.js

---
+++ OrbitControls.js
@@ -0,0 +1,16 @@
+/*
+ * Skypack CDN - three@0.136.0
+ *
+ * Learn more:
+ *   📙 Package Documentation: https://www.skypack.dev/view/three
+ *   📘 Skypack Documentation: https://www.skypack.dev/docs
+ *
+ * Pinned URL: (Optimized for Production)
+ *   ▶️ Normal: https://cdn.skypack.dev/pin/three@v0.136.0-4Px7Kx1INqCFBN0tXUQc/mode=imports/unoptimized/examples/jsm/controls/OrbitControls.js
+ *   ⏩ Minified: https://cdn.skypack.dev/pin/three@v0.136.0-4Px7Kx1INqCFBN0tXUQc/mode=imports,min/unoptimized/examples/jsm/controls/OrbitControls.js
+ *
+ */
+
+// Browser-Optimized Imports (Don't directly import the URLs below in your application!)
+export * from '/-/three@v0.136.0-4Px7Kx1INqCFBN0tXUQc/dist=es2019,mode=imports/unoptimized/examples/jsm/controls/OrbitControls.js';
+export {default} from '/-/three@v0.136.0-4Px7Kx1INqCFBN0tXUQc/dist=es2019,mode=imports/unoptimized/examples/jsm/controls/OrbitControls.js';
